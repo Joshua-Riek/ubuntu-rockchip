b1dd9e0c2ee9ff9d4a2d4eb67d79347e6f3e6e4a
Joshua Riek <jjriek@verizon.net>
2023-01-27 11:16:17 -0500

    rework image build script

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -19,104 +19,111 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 filename.rootfs.tar.xz"
+    exit 1
+fi
+
+rootfs="$(readlink -f "$1")"
+if [[ "$(basename "${rootfs}")" != *".rootfs.tar.xz" || ! -e "${rootfs}" ]]; then
+    echo "Error: $(basename "${rootfs}") must be a rootfs tarfile"
+    exit 1
+fi
+
 cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
 mkdir -p images build && cd build
 
-for rootfs in *.rootfs.tar.xz; do
-    if [ ! -e "${rootfs}" ]; then
-        echo "Error: could not find any rootfs tarfile, please run build-rootfs.sh"
-        exit 1
-    fi
+# Create an empty disk image
+img="../images/$(basename "${rootfs}" .rootfs.tar.xz).img"
+size="$(xz -l "${rootfs}" | tail -n +2 | sed 's/,//g' | awk '{print int($5 + 1)}')"
+truncate -s "$(( size + 2048 + 512 ))M" "${img}"
 
-    # Create an empty disk image
-    img="../images/$(basename "${rootfs}" .rootfs.tar.xz).img"
-    size="$(xz -l "${rootfs}" | tail -n +2 | sed 's/,//g' | awk '{print int($5 + 1)}')"
-    truncate -s "$(( size + 2048 + 512 ))M" "${img}"
+# Create loop device for disk image
+loop="$(losetup -f)"
+losetup "${loop}" "${img}"
+disk="${loop}"
 
-    # Create loop device for disk image
-    loop="$(losetup -f)"
-    losetup "${loop}" "${img}"
-    disk="${loop}"
+# Ensure disk is not mounted
+mount_point=/tmp/mnt
+umount "${disk}"* 2> /dev/null || true
+umount ${mount_point}/* 2> /dev/null || true
+mkdir -p ${mount_point}
 
-    # Ensure disk is not mounted
-    mount_point=/tmp/mnt
-    umount "${disk}"* 2> /dev/null || true
-    umount ${mount_point}/* 2> /dev/null || true
-    mkdir -p ${mount_point}
+# Setup partition table
+dd if=/dev/zero of="${disk}" count=4096 bs=512
+parted --script "${disk}" \
+mklabel gpt \
+mkpart primary fat32 64MiB 512MiB \
+mkpart primary ext4 512MiB 100%
 
-    # Setup partition table
-    dd if=/dev/zero of="${disk}" count=4096 bs=512
-    parted --script "${disk}" \
-    mklabel gpt \
-    mkpart primary fat32 64MiB 512MiB \
-    mkpart primary ext4 512MiB 100%
+set +e
 
-    set +e
+# Create partitions
+fdisk "${disk}" << EOF
+t
+1
+1
+t
+2
+20
+w
+EOF
 
-    # Create partitions
-    (
-    echo t
-    echo 1
-    echo 1
-    echo t
-    echo 2
-    echo 20
-    echo w
-    ) | fdisk "${disk}"
+set -eE
 
-    set -eE
+partprobe "${disk}"
 
-    partprobe "${disk}"
+sleep 2
 
-    sleep 2
+# Generate random uuid for bootfs
+boot_uuid=$(uuidgen | head -c8)
 
-    # Generate random uuid for bootfs
-    boot_uuid=$(uuidgen | head -c8)
-
-    # Generate random uuid for rootfs
-    root_uuid=$(uuidgen)
+# Generate random uuid for rootfs
+root_uuid=$(uuidgen)
     
-    # Create filesystems on partitions
-    partition_char="$(if [[ ${disk: -1} == [0-9] ]]; then echo p; fi)"
-    mkfs.vfat -i "${boot_uuid}" -F32 -n boot "${disk}${partition_char}1"
-    dd if=/dev/zero of="${disk}${partition_char}2" bs=1KB count=10 > /dev/null
-    mkfs.ext4 -U "${root_uuid}" -L root "${disk}${partition_char}2"
+# Create filesystems on partitions
+partition_char="$(if [[ ${disk: -1} == [0-9] ]]; then echo p; fi)"
+mkfs.vfat -i "${boot_uuid}" -F32 -n boot "${disk}${partition_char}1"
+dd if=/dev/zero of="${disk}${partition_char}2" bs=1KB count=10 > /dev/null
+mkfs.ext4 -U "${root_uuid}" -L root "${disk}${partition_char}2"
 
-    # Mount partitions
-    mkdir -p ${mount_point}/{boot,root} 
-    mount "${disk}${partition_char}1" ${mount_point}/boot
-    mount "${disk}${partition_char}2" ${mount_point}/root
+# Mount partitions
+mkdir -p ${mount_point}/{boot,root} 
+mount "${disk}${partition_char}1" ${mount_point}/boot
+mount "${disk}${partition_char}2" ${mount_point}/root
 
-    # Copy the rootfs to root partition
-    echo -e "Decompressing $(basename "${rootfs}")\n"
-    tar -xpJf "${rootfs}" -C ${mount_point}/root
+# Copy the rootfs to root partition
+echo -e "Decompressing $(basename "${rootfs}")\n"
+tar -xpJf "${rootfs}" -C ${mount_point}/root
 
-    # Create fstab entries
-    mkdir -p ${mount_point}/root/boot/firmware
-    boot_uuid="${boot_uuid:0:4}-${boot_uuid:4:4}"
-    echo "# <file system>      <mount point>  <type>  <options>   <dump>  <fsck>" > ${mount_point}/root/etc/fstab
-    echo "UUID=${boot_uuid^^}  /boot/firmware vfat    defaults    0       2" >> ${mount_point}/root/etc/fstab
-    echo "UUID=${root_uuid,,}  /              ext4    defaults    0       1" >> ${mount_point}/root/etc/fstab
-    echo "/swapfile            none           swap    sw          0       0" >> ${mount_point}/root/etc/fstab
+# Create fstab entries
+mkdir -p ${mount_point}/root/boot/firmware
+boot_uuid="${boot_uuid:0:4}-${boot_uuid:4:4}"
+cat > ${mount_point}/root/etc/fstab << EOF
+# <file system>      <mount point>  <type>  <options>   <dump>  <fsck>
+UUID=${boot_uuid^^}  /boot/firmware vfat    defaults    0       2
+UUID=${root_uuid,,}  /              ext4    defaults    0       1
+/swapfile            none           swap    sw          0       0
+EOF
 
-    # Extract grub arm64-efi to host system 
-    if [ ! -d "/usr/lib/grub/arm64-efi" ]; then
-        rm -f /usr/lib/grub/arm64-efi
-        ln -s ${mount_point}/root/usr/lib/grub/arm64-efi /usr/lib/grub/arm64-efi
-    fi
+# Extract grub arm64-efi to host system 
+if [ ! -d "/usr/lib/grub/arm64-efi" ]; then
+    rm -f /usr/lib/grub/arm64-efi
+    ln -s ${mount_point}/root/usr/lib/grub/arm64-efi /usr/lib/grub/arm64-efi
+fi
 
-    # Install grub 
-    mkdir -p ${mount_point}/boot/efi/boot
-    mkdir -p ${mount_point}/boot/boot/grub
-    grub-install --target=arm64-efi --efi-directory=${mount_point}/boot --boot-directory=${mount_point}/boot/boot --removable --recheck
+# Install grub 
+mkdir -p ${mount_point}/boot/efi/boot
+mkdir -p ${mount_point}/boot/boot/grub
+grub-install --target=arm64-efi --efi-directory=${mount_point}/boot --boot-directory=${mount_point}/boot/boot --removable --recheck
 
-    # Remove grub arm64-efi if extracted
-    if [ -L "/usr/lib/grub/arm64-efi" ]; then
-        rm -f /usr/lib/grub/arm64-efi
-    fi
+# Remove grub arm64-efi if extracted
+if [ -L "/usr/lib/grub/arm64-efi" ]; then
+    rm -f /usr/lib/grub/arm64-efi
+fi
 
-    # Grub config
-    cat > ${mount_point}/boot/boot/grub/grub.cfg << EOF
+# Grub config
+cat > ${mount_point}/boot/boot/grub/grub.cfg << EOF
 insmod gzio
 set background_color=black
 set default=0
@@ -131,8 +138,8 @@ menuentry 'Boot' {
 }
 EOF
 
-    # Uboot script
-    cat > ${mount_point}/boot/boot.cmd << EOF
+# Uboot script
+cat > ${mount_point}/boot/boot.cmd << EOF
 env set bootargs "root=UUID=${root_uuid} console=ttyS2,1500000 console=tty1 rootfstype=ext4 rootwait rw"
 fatload \${devtype} \${devnum}:1 \${fdt_addr_r} /rk3588s-orangepi-5.dtb
 fdt addr \${fdt_addr_r} && fdt resize 0x2000
@@ -143,31 +150,30 @@ unzip \${ramdisk_addr_r} \${kernel_addr_r} \${filesize}
 ext4load \${devtype} \${devnum}:2 \${ramdisk_addr_r} /boot/initrd.img
 booti \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r}
 EOF
-    mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d ${mount_point}/boot/boot.cmd ${mount_point}/boot/boot.scr
+mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d ${mount_point}/boot/boot.cmd ${mount_point}/boot/boot.scr
 
-    # Copy device tree blobs
-    mkdir -p ${mount_point}/boot/overlays
-    cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/rk3588s-orangepi-5.dtb ${mount_point}/boot
+# Copy device tree blobs
+mkdir -p ${mount_point}/boot/overlays
+cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/rk3588s-orangepi-5.dtb ${mount_point}/boot
 
-    # Copy device tree overlays
-    mkdir -p ${mount_point}/boot/overlays
-    cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/overlay/rk3588*.dtbo ${mount_point}/boot/overlays
+# Copy device tree overlays
+mkdir -p ${mount_point}/boot/overlays
+cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/overlay/rk3588*.dtbo ${mount_point}/boot/overlays
 
-    # Write bootloader to disk image
-    dd if=u-boot-orangepi/idbloader.img of="${loop}" seek=64 conv=notrunc
-    dd if=u-boot-orangepi/u-boot.itb of="${loop}" seek=16384 conv=notrunc
+# Write bootloader to disk image
+dd if=u-boot-orangepi/idbloader.img of="${loop}" seek=64 conv=notrunc
+dd if=u-boot-orangepi/u-boot.itb of="${loop}" seek=16384 conv=notrunc
 
-    sync --file-system
-    sync
+sync --file-system
+sync
 
-    # Umount partitions
-    umount "${disk}${partition_char}1"
-    umount "${disk}${partition_char}2"
+# Umount partitions
+umount "${disk}${partition_char}1"
+umount "${disk}${partition_char}2"
 
-    # Remove loop device
-    losetup -d "${loop}"
+# Remove loop device
+losetup -d "${loop}"
 
-    echo -e "\nCompressing $(basename "${img}.xz")\n"
-    xz -9 --extreme --force --keep --quiet --threads=0 "${img}"
-    rm -f "${img}"
-done
+echo -e "\nCompressing $(basename "${img}.xz")\n"
+xz -9 --extreme --force --keep --quiet --threads=0 "${img}"
+rm -f "${img}"
