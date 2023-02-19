881b843b42a98b3e47cf3d6c5f3a99477989def1
Joshua Riek <jjriek@verizon.net>
2023-02-18 20:11:46 -0500

    revert vops change and fix gdm hack

--- overlay/usr/lib/scripts/gdm-hack.sh
+++ overlay/usr/lib/scripts/gdm-hack.sh
@@ -3,6 +3,6 @@
 # Hack for GDM to restart on first HDMI hotplug
 if systemctl is-active --quiet gdm && grep -Fxq "WaylandEnable=true" /etc/gdm3/custom.conf; then
     if [ ! -f /tmp/gdm-wayland-session-hack.lock ]; then
-        pidof gdm-x-session > /dev/null && systemctl restart gdm && touch /tmp/gdm-wayland-session-hack.lock
+        touch /tmp/gdm-wayland-session-hack.lock && sleep 2 && pidof gdm-x-session > /dev/null && systemctl restart gdm
     fi
 fi
