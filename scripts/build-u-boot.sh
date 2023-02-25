fc17b2b02a5e4293232641badbfecc8f922e4824
Joshua Riek <jjriek@verizon.net>
2023-02-24 22:17:37 -0500

    add uboot spi images

--- scripts/build-u-boot.sh
+++ scripts/build-u-boot.sh
@@ -24,15 +24,54 @@ if [ ! -d u-boot-orangepi ]; then
 fi
 cd u-boot-orangepi
 
+# Set u-boot config with sata support
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- orangepi_5_sata_defconfig
+
+# Set custom u-boot version
+sed -i 's/CONFIG_LOCALVERSION=""/CONFIG_LOCALVERSION="-orange-pi"/g' .config
+touch .scmversion
+
+# Create u-boot binary with atf
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- -j "$(nproc)" BL31=../rkbin/bin/rk35/rk3588_bl31_v1.27.elf spl/u-boot-spl.bin u-boot.dtb u-boot.itb
+
+# Create secondary program loader
+./tools/mkimage -n rk3588 -T rksd -d ../rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.08.bin:spl/u-boot-spl.bin idbloader.img
+
+# Create spi bootloader image with sata support 
+dd if=/dev/zero of=rkspi_loader_sata.img bs=1M count=0 seek=16
+parted -s rkspi_loader_sata.img mklabel gpt
+parted -s rkspi_loader_sata.img unit s mkpart idbloader 64 7167
+parted -s rkspi_loader_sata.img unit s mkpart vnvm 7168 7679
+parted -s rkspi_loader_sata.img unit s mkpart reserved_space 7680 8063
+parted -s rkspi_loader_sata.img unit s mkpart reserved1 8064 8127
+parted -s rkspi_loader_sata.img unit s mkpart uboot_env 8128 8191
+parted -s rkspi_loader_sata.img unit s mkpart reserved2 8192 16383
+parted -s rkspi_loader_sata.img unit s mkpart uboot 16384 32734
+dd if=idbloader.img of=rkspi_loader_sata.img seek=64 conv=notrunc
+dd if=u-boot.itb of=rkspi_loader_sata.img seek=16384 conv=notrunc
+
 # Set u-boot config
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- orangepi_5_defconfig
 
-# Compile u-boot binary
-make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- -j "$(nproc)"
+# Set custom u-boot version
+sed -i 's/CONFIG_LOCALVERSION=""/CONFIG_LOCALVERSION="-orange-pi"/g' .config
+touch .scmversion
 
 # Create u-boot binary with atf
-cp "../rkbin/$(sed -n '/_bl31_/s/PATH=//p' ../rkbin/RKTRUST/RK3588TRUST.ini | tr -d '\r')" bl31.elf
-make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- -j "$(nproc)" u-boot.itb
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- -j "$(nproc)" BL31=../rkbin/bin/rk35/rk3588_bl31_v1.27.elf spl/u-boot-spl.bin u-boot.dtb u-boot.itb
 
 # Create secondary program loader
-./tools/mkimage -n rk3588 -T rksd -d "../rkbin/$(sed -n '/_ddr_/s/FlashData=//p' ../rkbin/RKBOOT/RK3588MINIALL.ini | tr -d '\r')":spl/u-boot-spl.bin idbloader.img
+./tools/mkimage -n rk3588 -T rksd -d ../rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.08.bin:spl/u-boot-spl.bin idbloader.img
+
+# Create spi bootloader image
+dd if=/dev/zero of=rkspi_loader.img bs=1M count=0 seek=16
+parted -s rkspi_loader.img mklabel gpt
+parted -s rkspi_loader.img unit s mkpart idbloader 64 7167
+parted -s rkspi_loader.img unit s mkpart vnvm 7168 7679
+parted -s rkspi_loader.img unit s mkpart reserved_space 7680 8063
+parted -s rkspi_loader.img unit s mkpart reserved1 8064 8127
+parted -s rkspi_loader.img unit s mkpart uboot_env 8128 8191
+parted -s rkspi_loader.img unit s mkpart reserved2 8192 16383
+parted -s rkspi_loader.img unit s mkpart uboot 16384 32734
+dd if=idbloader.img of=rkspi_loader.img seek=64 conv=notrunc
+dd if=u-boot.itb of=rkspi_loader.img seek=16384 conv=notrunc
