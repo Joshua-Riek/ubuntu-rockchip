9244e411899a8b40b07323f750205651e1ecdc9a
Joshua Riek <jjriek@verizon.net>
2023-03-10 10:35:14 -0500

    add mtd-tools

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -126,7 +126,7 @@ bash-completion man-db manpages nano gnupg initramfs-tools linux-firmware \
 ubuntu-drivers-common ubuntu-server dosfstools mtools parted ntfs-3g zip atop \
 p7zip-full htop iotop pciutils lshw lsof cryptsetup exfat-fuse hwinfo dmidecode \
 net-tools wireless-tools openssh-client openssh-server wpasupplicant ifupdown \
-pigz wget curl lm-sensors bluez gdisk i2c-tools u-boot-tools cloud-init
+pigz wget curl lm-sensors bluez gdisk i2c-tools u-boot-tools cloud-init mtd-tools
 
 # Clean package cache
 apt-get -y autoremove && apt-get -y clean && apt-get -y autoclean
