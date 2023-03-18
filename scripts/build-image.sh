28c10ecdb9395e7bb01fee2f6e7a7df9ce302f81
Joshua Riek <jjriek@verizon.net>
2023-03-18 11:31:47 -0400

    change root and boot partition names

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -53,8 +53,8 @@ mkdir -p ${mount_point}
 dd if=/dev/zero of="${disk}" count=4096 bs=512
 parted --script "${disk}" \
 mklabel gpt \
-mkpart primary fat32 32MiB 288MiB \
-mkpart primary ext4 288MiB 100%
+mkpart primary fat16 16MiB 272MiB \
+mkpart primary ext4 272MiB 100%
 
 set +e
 
@@ -62,7 +62,7 @@ set +e
 fdisk "${disk}" << EOF
 t
 1
-1
+30
 t
 2
 20
@@ -83,35 +83,39 @@ root_uuid=$(uuidgen)
     
 # Create filesystems on partitions
 partition_char="$(if [[ ${disk: -1} == [0-9] ]]; then echo p; fi)"
-mkfs.vfat -i "${boot_uuid}" -F32 -n boot "${disk}${partition_char}1"
+mkfs.vfat -i "${boot_uuid}" -F16 -n system-boot "${disk}${partition_char}1"
 dd if=/dev/zero of="${disk}${partition_char}2" bs=1KB count=10 > /dev/null
-mkfs.ext4 -U "${root_uuid}" -L root "${disk}${partition_char}2"
+mkfs.ext4 -U "${root_uuid}" -L writable "${disk}${partition_char}2"
 
 # Mount partitions
-mkdir -p ${mount_point}/{boot,root} 
-mount "${disk}${partition_char}1" ${mount_point}/boot
-mount "${disk}${partition_char}2" ${mount_point}/root
+mkdir -p ${mount_point}/{system-boot,writable} 
+mount "${disk}${partition_char}1" ${mount_point}/system-boot
+mount "${disk}${partition_char}2" ${mount_point}/writable
 
 # Copy the rootfs to root partition
 echo -e "Decompressing $(basename "${rootfs}")\n"
-tar -xpJf "${rootfs}" -C ${mount_point}/root
+tar -xpJf "${rootfs}" -C ${mount_point}/writable
 
 # Set boot args for the splash screen
 [ -z "${img##*desktop*}" ] && bootargs="quiet splash plymouth.ignore-serial-consoles" || bootargs=""
 
 # Create fstab entries
-mkdir -p ${mount_point}/root/boot/firmware
-boot_uuid="${boot_uuid:0:4}-${boot_uuid:4:4}"
-cat > ${mount_point}/root/etc/fstab << EOF
-# <file system>      <mount point>  <type>  <options>   <dump>  <fsck>
-UUID=${boot_uuid^^}  /boot/firmware vfat    defaults    0       2
-UUID=${root_uuid,,}  /              ext4    defaults    0       1
-/swapfile            none           swap    sw          0       0
+mkdir -p ${mount_point}/writable/boot/firmware
+cat > ${mount_point}/writable/etc/fstab << EOF
+# <file system>     <mount point>  <type>  <options>   <dump>  <fsck>
+LABEL=system-boot   /boot/firmware vfat    defaults    0       2
+LABEL=writable      /              ext4    defaults    0       1
+/swapfile           none           swap    sw          0       0
 EOF
 
 # Uboot script
-cat > ${mount_point}/boot/boot.cmd << EOF
-env set bootargs "root=UUID=${root_uuid} console=ttyS2,1500000 console=tty1 cma=64M rootfstype=ext4 rootwait rw cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1 systemd.unified_cgroup_hierarchy=0 ${bootargs}"
+cat > ${mount_point}/system-boot/boot.cmd << EOF
+# This is a boot script for U-Boot
+#
+# Recompile with:
+# mkimage -A arm64 -O linux -T script -C none -n "Boot Script" boot.cmd boot.scr
+
+env set bootargs "console=ttyS2,1500000 console=tty1 root=LABEL=writable rootfstype=ext4 rootwait rw cma=64M cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1 systemd.unified_cgroup_hierarchy=0 ${bootargs}"
 
 load \${devtype} \${devnum}:1 \${fdt_addr_r} /rk3588s-orangepi-5.dtb
 fdt addr \${fdt_addr_r} && fdt resize 0x10000
@@ -136,30 +140,30 @@ load \${devtype} \${devnum}:2 \${ramdisk_addr_r} /boot/initrd.img
 
 booti \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r}
 EOF
-mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d ${mount_point}/boot/boot.cmd ${mount_point}/boot/boot.scr
+mkimage -A arm64 -O linux -T script -C none -n "Boot Script" -d ${mount_point}/system-boot/boot.cmd ${mount_point}/system-boot/boot.scr
 
 # Device tree overlays to load
-echo "overlays=" > ${mount_point}/boot/overlays.txt
+echo "overlays=" > ${mount_point}/system-boot/overlays.txt
 
 # Copy device tree blobs
-mkdir -p ${mount_point}/boot/overlays
-cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/rk3588s-orangepi-5.dtb ${mount_point}/boot
+mkdir -p ${mount_point}/system-boot/overlays
+cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/rk3588s-orangepi-5.dtb ${mount_point}/system-boot
 
 # Copy device tree overlays
-mkdir -p ${mount_point}/boot/overlays
-cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/overlay/rk3588*.dtbo ${mount_point}/boot/overlays
+mkdir -p ${mount_point}/system-boot/overlays
+cp -r linux-orangepi/arch/arm64/boot/dts/rockchip/overlay/rk3588*.dtbo ${mount_point}/system-boot/overlays
 
 # Write bootloader to disk image
 dd if=u-boot-orangepi/idbloader.img of="${loop}" seek=64 conv=notrunc
 dd if=u-boot-orangepi/u-boot.itb of="${loop}" seek=16384 conv=notrunc
 
 # Copy spi bootloader to disk image
-mkdir -p ${mount_point}/root/usr/share/orangepi
-cp u-boot-orangepi/rkspi_loader.img ${mount_point}/root/usr/share/orangepi/rkspi_loader.img
-cp u-boot-orangepi/rkspi_loader_sata.img ${mount_point}/root/usr/share/orangepi/rkspi_loader_sata.img
+mkdir -p ${mount_point}/writable/usr/share/orangepi
+cp u-boot-orangepi/rkspi_loader.img ${mount_point}/writable/usr/share/orangepi/rkspi_loader.img
+cp u-boot-orangepi/rkspi_loader_sata.img ${mount_point}/writable/usr/share/orangepi/rkspi_loader_sata.img
 
 # Cloud init config for server image
-[ -z "${img##*server*}" ] && cp ../overlay/boot/firmware/{meta-data,user-data,network-config} ${mount_point}/boot
+[ -z "${img##*server*}" ] && cp ../overlay/boot/firmware/{meta-data,user-data,network-config} ${mount_point}/system-boot
 
 sync --file-system
 sync
