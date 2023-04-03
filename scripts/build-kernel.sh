149784ef0663a086f1f8ec7516749edb2cefb05f
Joshua Riek <jjriek@verizon.net>
2023-04-03 15:29:15 -0400

    rename kernel patches

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -11,16 +11,11 @@ fi
 cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
 mkdir -p build && cd build
 
-# Download the orange pi linux kernel source
+# Download and patch the orange pi linux kernel source
 if [ ! -d linux-orangepi ]; then
     git clone --progress -b orange-pi-5.10-rk3588 https://github.com/orangepi-xunlong/linux-orangepi.git
     git -C linux-orangepi checkout 88961a71100e64a97124a674eff8b71863d4cbbc
-    git -C linux-orangepi apply ../../patches/linux-orangepi/0001-Revert-ANDROID-clk-Enable-writable-debugfs-files.patch
-    git -C linux-orangepi apply ../../patches/linux-orangepi/0002-debianize-kernel-package.patch
-    git -C linux-orangepi apply ../../patches/linux-orangepi/0003-hdmi-sound-improvements.patch
-    git -C linux-orangepi apply ../../patches/linux-orangepi/0004-fix-dw-dp-warning-msg.patch
-    git -C linux-orangepi apply ../../patches/linux-orangepi/0005-enable-dmc.patch
-    git -C linux-orangepi apply ../../patches/linux-orangepi/0006-suppress-unsupported-format-modifier.patch
+    for patch in ../patches/linux-orangepi/*.patch; do git -C linux-orangepi apply "../${patch}"; done
 fi
 cd linux-orangepi
 
