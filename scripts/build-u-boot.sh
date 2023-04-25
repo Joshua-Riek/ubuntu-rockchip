b9a8bc0c70204f0b5f1907b319381a12f23aea98
Joshua Riek <jjriek@verizon.net>
2023-04-25 16:11:35 -0400

    create debian repo for u-boot and kernel

--- scripts/build-u-boot.sh
+++ scripts/build-u-boot.sh
@@ -18,8 +18,17 @@ fi
 
 # Download the orangepi u-boot source
 if [ ! -d u-boot-orangepi ]; then
-    git clone --progress -b v2017.09-rk3588 https://github.com/Joshua-Riek/u-boot-orangepi.git
-    git -C u-boot-orangepi checkout fd734ab8c51f4bd4fb2b2a3a683dbc9a791ea087
+    git clone https://github.com/Joshua-Riek/u-boot-orangepi-debian.git
+    git -C u-boot-orangepi-debian checkout 53ec02881ff0a48719fb0b0c636eb43e657eeb6d
+
+    # shellcheck source=/dev/null
+    source u-boot-orangepi-debian/upstream
+    git clone --single-branch --progress -b "${BRANCH}" "${GIT}"
+    git -C u-boot-orangepi checkout "${COMMIT}"
+    mv u-boot-orangepi-debian u-boot-orangepi/debian
+    for patch in u-boot-orangepi/debian/patches/*.patch; do
+        git -C u-boot-orangepi apply "$(readlink -f "${patch}")"
+    done
 fi
 cd u-boot-orangepi
 
@@ -27,6 +36,5 @@ cd u-boot-orangepi
 export $(dpkg-architecture -aarm64)
 
 # Compile u-boot into a deb package
-CROSS_COMPILE=aarch64-linux-gnu- ./debian/rules clean
 CROSS_COMPILE=aarch64-linux-gnu- ./debian/rules build-"${BOARD}"
 CROSS_COMPILE=aarch64-linux-gnu- ./debian/rules binary-"${BOARD}"
