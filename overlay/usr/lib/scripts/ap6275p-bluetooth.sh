e8734a25ab096dee91707082286afb2f73a31b32
Joshua Riek <jjriek@verizon.net>
2023-04-27 19:51:13 -0400

    use hardware mac address for bluetooth #112

--- overlay/usr/lib/scripts/ap6275p-bluetooth.sh
+++ overlay/usr/lib/scripts/ap6275p-bluetooth.sh
@@ -3,6 +3,6 @@
 bt_status=$(cat /proc/device-tree/wireless-bluetooth/status)
 if [[ ${bt_status} == "okay" ]]; then
     rfkill unblock all
-    brcm_patchram_plus --bd_addr_rand --enable_hci --no2bytes --use_baudrate_for_download --tosleep 200000 \
+    brcm_patchram_plus --enable_hci --no2bytes --use_baudrate_for_download --tosleep 200000 \
         --baudrate 1500000 --patchram /lib/firmware/ap6275p/BCM4362A2.hcd /dev/ttyS9 &
 fi
