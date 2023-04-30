36fc6f87edca0190fe60260c3d0effcae4e98acb
Joshua Riek <jjriek@verizon.net>
2023-04-30 15:13:10 -0400

    add rfkill package

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -139,7 +139,7 @@ ubuntu-drivers-common ubuntu-server dosfstools mtools parted ntfs-3g zip atop \
 p7zip-full htop iotop pciutils lshw lsof landscape-common exfat-fuse hwinfo \
 net-tools wireless-tools openssh-client openssh-server wpasupplicant ifupdown \
 pigz wget curl lm-sensors bluez gdisk usb-modeswitch usb-modeswitch-data make \
-gcc libc6-dev bison libssl-dev flex wiringpi-opi flash-kernel fake-hwclock
+gcc libc6-dev bison libssl-dev flex wiringpi-opi flash-kernel fake-hwclock rfkill
 
 # Remove cryptsetup and needrestart
 apt-get -y remove cryptsetup needrestart
