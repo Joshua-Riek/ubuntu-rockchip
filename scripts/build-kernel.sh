52b4022f6c392f823c3d6b23473c2593e09bf663
Joshua Riek <jjriek@verizon.net>
2023-05-15 17:56:08 -0400

    use linux-5.10-gen-rkr3.6 kernel fork

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -11,24 +11,12 @@ fi
 cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
 mkdir -p build && cd build
 
-if [[ -z ${BOARD} ]]; then
-    echo "Error: BOARD is not set"
-    exit 1
+if [ ! -d linux-rockchip ]; then
+    git clone --single-branch --progress -b linux-5.10-gen-rkr3.6 https://github.com/Joshua-Riek/linux-rockchip.git linux-rockchip
+    git -C linux-rockchip checkout 4604f673957a2cdcb71547ca1dbc82781a8b3118
+    cp -r ../packages/linux-rockchip/debian linux-rockchip
 fi
-
-if [[ -z ${VENDOR} ]]; then
-    echo "Error: VENDOR is not set"
-    exit 1
-fi
-
-if [ ! -d linux-"${VENDOR}" ]; then
-    # shellcheck source=/dev/null
-    source ../packages/linux-"${VENDOR}"/debian/upstream
-    git clone --single-branch --progress -b "${BRANCH}" "${GIT}" linux-"${VENDOR}"
-    git -C linux-"${VENDOR}" checkout "${COMMIT}"
-    cp -r ../packages/linux-"${VENDOR}"/debian linux-"${VENDOR}"
-fi
-cd linux-"${VENDOR}"
+cd linux-rockchip
 
 # Compile kernel into a deb package
 dpkg-buildpackage -a "$(cat debian/arch)" -d -b -nc -uc
