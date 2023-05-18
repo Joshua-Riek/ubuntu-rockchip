63cede0b2b36c1ef26061302adba64c2def225a7
Joshua Riek <jjriek@verizon.net>
2023-05-17 22:00:44 -0400

    add rock5a audio rules

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -277,7 +277,11 @@ if [[ ${BOARD} =~ orangepi5|orangepi5b ]]; then
 
     chroot ${chroot_dir} /bin/bash -c "apt-get -y install wiringpi-opi"
     echo "BOARD=${BOARD}" > ${chroot_dir}/etc/"${VENDOR}"-release
-elif [[ "${BOARD}" =~ rock5b|rock5a ]]; then
+elif [[ "${BOARD}" =~ rock5a ]]; then
+	echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi0-sound", ENV{SOUND_DESCRIPTION}="HDMI0 Audio"' > ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
+	echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi1-sound", ENV{SOUND_DESCRIPTION}="HDMI1 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
+	echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-es8316-sound", ENV{SOUND_DESCRIPTION}="ES8316 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
+elif [[ "${BOARD}" =~ rock5b ]]; then
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi0-sound", ENV{SOUND_DESCRIPTION}="HDMI0 Audio"' > ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi1-sound", ENV{SOUND_DESCRIPTION}="HDMI1 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmiin-sound", ENV{SOUND_DESCRIPTION}="HDMI-In Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
