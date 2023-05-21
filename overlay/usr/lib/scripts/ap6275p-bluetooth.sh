c221f1f04e500c6d4fb2a44584f6e75fd72ac71e
Joshua Riek <jjriek@verizon.net>
2023-05-21 00:37:35 -0400

    improve ap6275p bluetooth script

--- overlay/usr/lib/scripts/ap6275p-bluetooth.sh
+++ overlay/usr/lib/scripts/ap6275p-bluetooth.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 bt_status=$(cat /proc/device-tree/wireless-bluetooth/status)
-if [[ ${bt_status} == "okay" ]]; then
+wifi_chip=$(cat /proc/device-tree/wireless-wlan/wifi_chip_type)
+if [[ ${wifi_chip} == "ap6275p" && ${bt_status} == "okay" ]]; then
     rfkill unblock all
     brcm_patchram_plus --enable_hci --no2bytes --use_baudrate_for_download --tosleep 200000 \
         --baudrate 1500000 --patchram /lib/firmware/ap6275p/BCM4362A2.hcd /dev/ttyS9 &
