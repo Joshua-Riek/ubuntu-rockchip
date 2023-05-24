3eb623b580535ffefdb2fba2f837293e1efe6d1e
Joshua Riek <jjriek@verizon.net>
2023-05-24 15:32:54 -0400

    fix rock5a audio config

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -294,7 +294,7 @@ elif [[ "${BOARD}" =~ orangepi5plus ]]; then
     echo "BOARD=${BOARD}" > ${chroot_dir}/etc/"${VENDOR}"-release
 elif [[ "${BOARD}" =~ rock5a ]]; then
 	echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi0-sound", ENV{SOUND_DESCRIPTION}="HDMI0 Audio"' > ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
-	echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi1-sound", ENV{SOUND_DESCRIPTION}="HDMI1 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
+	echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-dp0-sound", ENV{SOUND_DESCRIPTION}="DP0 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
 	echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-es8316-sound", ENV{SOUND_DESCRIPTION}="ES8316 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
 elif [[ "${BOARD}" =~ rock5b ]]; then
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi0-sound", ENV{SOUND_DESCRIPTION}="HDMI0 Audio"' > ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
