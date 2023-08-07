f24247d25b555bea47056783011ca1342774cbf4
Joshua Riek <jjriek@verizon.net>
2023-08-06 23:49:04 -0400

    add alsa audio config service to fix rock 5 audio issues #291

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -260,7 +260,7 @@ libdvbv5-0 libdvbv5-dev libdvbv5-doc libv4l-0 libv4l2rds0 libv4lconvert0 libv4l-
 libv4l-rkmpp qv4l2 v4l-utils librockchip-mpp1 librockchip-mpp-dev librockchip-vpu0 \
 rockchip-mpp-demos librga2 librga-dev libegl-mesa0 libegl1-mesa-dev libgbm-dev \
 libgl1-mesa-dev libgles2-mesa-dev libglx-mesa0 mesa-common-dev mesa-vulkan-drivers \
-mesa-utils libwidevinecdm
+mesa-utils libwidevinecdm libcanberra-pulse
 
 # Remove cloud-init and landscape-common
 apt-get -y purge cloud-init landscape-common
