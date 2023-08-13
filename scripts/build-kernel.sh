1f36f15ed42a589858eb9105edbefadc068c8459
Joshua-Riek <jjriek@verizon.net>
2023-08-12 21:09:46 -0400

    mainline 6.x kernel build test

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -11,12 +11,27 @@ fi
 cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
 mkdir -p build && cd build
 
-if [ ! -d linux-rockchip ]; then
-    git clone --single-branch --progress -b linux-5.10-gen-rkr4 https://github.com/Joshua-Riek/linux-rockchip.git linux-rockchip
+if [[ 1 == 1 ]]; then
+    test -d linux-rockchip || git clone --single-branch --progress -b  linux-5.10-gen-rkr4 https://github.com/Joshua-Riek/linux-rockchip.git linux-rockchip
+    cd linux-rockchip
+
+    # Compile kernel into a deb package
+    dpkg-buildpackage -a "$(cat debian/arch)" -d -b -nc -uc
+
+    rm -f ../*.buildinfo ../*.changes
+else
+    test -d linux ||  git clone --single-branch --progress -b rk3588-test https://github.com/Joshua-Riek/linux.git --depth=100
+    cd linux
+
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- linux-rockchip-rk3588-collabora_defconfig
+    ./scripts/config --disable CONFIG_DEBUG_INFO
+    ./scripts/config --disable DEBUG_INFO
+
+    echo "0" > .version
+    echo "" > .scmversion
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg -j16
+
+    rm -f ../linux-image-*dbg*.deb
+    rm -f ../linux-libc-dev_*.deb
+    rm -f ../*.buildinfo ../*.changes ../*.dsc ../*.tar.gz
 fi
-cd linux-rockchip
-
-# Compile kernel into a deb package
-dpkg-buildpackage -a "$(cat debian/arch)" -d -b -nc -uc
-
-rm -f ../*.buildinfo ../*.changes
