3f3f7d5ff4454048abc916d8a53d78ac245d9166
Joshua-Riek <jjriek@verizon.net>
2023-09-07 21:54:29 -0400

    updated camera engine from rockchip

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -255,9 +255,11 @@ mount -o bind /dev/pts ${chroot_dir}/dev/pts
 
 # Install rkaiq
 cp -r ../packages/rkaiq/camera-engine-rkaiq_rk3588_arm64.deb ${chroot_dir}/tmp
+cp -r ../packages/rkaiq/camera_engine_rkisp-v2.2.0_arm64.deb ${chroot_dir}/tmp
 chroot ${chroot_dir} /bin/bash -c "dpkg -i /tmp/camera-engine-rkaiq_rk3588_arm64.deb"
-cp -f ../packages/rkaiq/rkaiq_3A_server ${chroot_dir}/usr/bin
+chroot ${chroot_dir} /bin/bash -c "dpkg -i /tmp/camera_engine_rkisp-v2.2.0_arm64.deb"
 rm -f ${chroot_dir}/tmp/camera-engine-rkaiq_rk3588_arm64.deb
+rm -f ${chroot_dir}/tmp/camera_engine_rkisp-v2.2.0_arm64.deb
 
 # Download and update packages
 cat << EOF | chroot ${chroot_dir} /bin/bash
