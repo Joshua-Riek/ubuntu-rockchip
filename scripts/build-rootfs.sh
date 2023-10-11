2e9c943a3475cdb932ac6c823652319c07ac35b3
Joshua-Riek <jjriek@verizon.net>
2023-10-11 19:57:50 -0400

    do not set cpu or gpu governor to performance on boot

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -196,14 +196,6 @@ cp ${overlay_dir}/usr/lib/scripts/resize-filesystem.sh ${chroot_dir}/usr/lib/scr
 cp ${overlay_dir}/usr/lib/systemd/system/resize-filesystem.service ${chroot_dir}/usr/lib/systemd/system/resize-filesystem.service
 chroot ${chroot_dir} /bin/bash -c "systemctl enable resize-filesystem"
 
-# Set cpu governor to performance
-cp ${overlay_dir}/usr/lib/systemd/system/cpu-governor-performance.service ${chroot_dir}/usr/lib/systemd/system/cpu-governor-performance.service
-chroot ${chroot_dir} /bin/bash -c "systemctl enable cpu-governor-performance"
-
-# Set gpu governor to performance
-cp ${overlay_dir}/usr/lib/systemd/system/gpu-governor-performance.service ${chroot_dir}/usr/lib/systemd/system/gpu-governor-performance.service
-chroot ${chroot_dir} /bin/bash -c "systemctl enable gpu-governor-performance"
-
 # Add realtek bluetooth firmware to initrd 
 cp ${overlay_dir}/usr/share/initramfs-tools/hooks/rtl-bt ${chroot_dir}/usr/share/initramfs-tools/hooks/rtl-bt
 
