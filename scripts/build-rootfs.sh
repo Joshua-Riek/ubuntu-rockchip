2596c04bcf9d15a8085c0b1ac5b8845c0b7349e8
Joshua-Riek <jjriek@verizon.net>
2023-10-12 17:40:08 -0400

    add uuid-runtime package to rootfs

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -136,7 +136,8 @@ ubuntu-drivers-common ubuntu-server dosfstools mtools parted ntfs-3g zip atop \
 p7zip-full htop iotop pciutils lshw lsof landscape-common exfat-fuse hwinfo \
 net-tools wireless-tools openssh-client openssh-server wpasupplicant ifupdown \
 pigz wget curl lm-sensors bluez gdisk usb-modeswitch usb-modeswitch-data make \
-gcc libc6-dev bison libssl-dev flex flash-kernel fake-hwclock wireless-regdb
+gcc libc6-dev bison libssl-dev flex flash-kernel fake-hwclock wireless-regdb \
+uuid-runtime
 
 # Remove cryptsetup and needrestart
 apt-get -y remove cryptsetup needrestart
