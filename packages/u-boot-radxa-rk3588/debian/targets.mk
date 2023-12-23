62f472ff2be6494c09b7d4a3ad4301ef89104440
Joshua Riek <jjriek@verizon.net>
2023-12-23 12:36:00 -0500

    squash lubancat and friendlyelec u-boot packages into radxa

---
+++ packages/u-boot-radxa-rk3588/debian/targets.mk
@@ -0,0 +1,42 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += rock-5b-rk3588
+rock-5b-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+rock-5b-rk3588_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += rock-5a-rk3588s
+rock-5a-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+rock-5a-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += rock-5a-spi-rk3588s
+rock-5a-spi-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+rock-5a-spi-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += rock-5-itx-rk3588
+rock-5-itx-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+rock-5-itx-rk3588_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += radxa-cm5-io-rk3588s
+radxa-cm5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+radxa-cm5-io-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += radxa-nx5-io-rk3588s
+radxa-nx5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+radxa-nx5-io-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += nanopi-r6s-rk3588s
+nanopi-r6s-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+nanopi-r6s-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += nanopi-r6c-rk3588s
+nanopi-r6c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+nanopi-r6c-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += nanopc-t6-rk3588
+nanopc-t6-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+nanopc-t6-rk3588_bl31 := rk3588_bl31_v1.38.elf
+
+u-boot-rockchip_platforms += lubancat-4-rk3588s
+lubancat-4-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+lubancat-4-rk3588s_bl31 := rk3588_bl31_v1.38.elf
