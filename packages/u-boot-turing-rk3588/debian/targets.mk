02f2c2e0f80900a600920aaa377f548dc2e3a75c
Joshua Riek <jjriek@verizon.net>
2023-12-24 10:07:06 -0500

    cleanup turing u-boot package and prep for release

---
+++ packages/u-boot-turing-rk3588/debian/targets.mk
@@ -0,0 +1,7 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += turing-rk1-rk3588
+turing-rk1-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_uart9_115200_v1.11.bin
+turing-rk1-rk3588_bl31 := rk3588_bl31_v1.38.elf
+turing-rk1-rk3588_pkg := turing-rk1
