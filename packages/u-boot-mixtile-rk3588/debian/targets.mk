584c4355e1e01d182ccc76f858849a2154dcaea0
Joshua Riek <jjriek@verizon.net>
2023-12-31 14:54:49 -0500

    cleanup mixtile blade3 u-boot

---
+++ packages/u-boot-mixtile-rk3588/debian/targets.mk
@@ -0,0 +1,7 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += mixtile-blade3-rk3588
+mixtile-blade3-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
+mixtile-blade3-rk3588_bl31 := rk3588_bl31_v1.38.elf
+mixtile-blade3-rk3588_pkg := mixtile-blade3
