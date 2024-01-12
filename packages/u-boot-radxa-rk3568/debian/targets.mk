0350abab5a245fcc614608a5b8302ab63a1f3616
Joshua Riek <jjriek@verizon.net>
2024-01-11 19:15:06 -0500

    add support for the Radxa Zero 3

---
+++ packages/u-boot-radxa-rk3568/debian/targets.mk
@@ -0,0 +1,7 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += radxa-zero3-rk3566
+radxa-zero3-rk3566_ddr := rk3568_ddr_1560MHz_v1.18.bin
+radxa-zero3-rk3566_bl31 := rk3568_bl31_v1.43.elf
+radxa-zero3-rk3566_pkg := radxa-zero3
