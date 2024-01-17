0b4600c077804bb01034e5cbcd636d70a27541bf
Joshua Riek <jjriek@verizon.net>
2024-01-17 17:03:44 -0500

    update ddr and spl for the radxa zero 3

--- packages/u-boot-radxa-rk3568/debian/targets.mk
+++ packages/u-boot-radxa-rk3568/debian/targets.mk
@@ -2,6 +2,6 @@
 # debian/rules includes this Makefile snippet.
 
 u-boot-rockchip_platforms += radxa-zero3-rk3566
-radxa-zero3-rk3566_ddr := rk3568_ddr_1560MHz_v1.18.bin
-radxa-zero3-rk3566_bl31 := rk3568_bl31_v1.43.elf
+radxa-zero3-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
+radxa-zero3-rk3566_bl31 := rk3568_bl31_set_pmic_sleep_low_20221018_v1.32.elf
 radxa-zero3-rk3566_pkg := radxa-zero3
