d176ba6be593f551c58d7723ddd18a64d08f1094
Joshua Riek <jjriek@verizon.net>
2024-01-19 19:35:19 -0500

    add support for the orangepi 3b

--- packages/u-boot-turing-rk3588/debian/targets.mk
+++ packages/u-boot-turing-rk3588/debian/targets.mk
@@ -5,3 +5,8 @@ u-boot-rockchip_platforms += turing-rk1-rk3588
 turing-rk1-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_uart9_115200_v1.11.bin
 turing-rk1-rk3588_bl31 := rk3588_bl31_v1.38.elf
 turing-rk1-rk3588_pkg := turing-rk1
+
+u-boot-rockchip_platforms += orangepi-3b-rk3566
+orangepi-3b-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
+orangepi-3b-rk3566_bl31 := rk3568_bl31_v1.43.elf
+orangepi-3b-rk3566_pkg := orangepi-3b
