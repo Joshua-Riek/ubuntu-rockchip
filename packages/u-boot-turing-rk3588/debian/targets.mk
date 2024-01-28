01d294a6f067839e6fb8b3fb0d890f7538ea09da
Joshua Riek <jjriek@verizon.net>
2024-01-28 13:19:44 -0500

    add the radxa zero3 to u-boot 2024.01

--- packages/u-boot-turing-rk3588/debian/targets.mk
+++ packages/u-boot-turing-rk3588/debian/targets.mk
@@ -10,3 +10,8 @@ u-boot-rockchip_platforms += orangepi-3b-rk3566
 orangepi-3b-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
 orangepi-3b-rk3566_bl31 := rk3568_bl31_v1.43.elf
 orangepi-3b-rk3566_pkg := orangepi-3b
+
+u-boot-rockchip_platforms += radxa-zero3-rk3566
+radxa-zero3-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
+radxa-zero3-rk3566_bl31 := rk3568_bl31_v1.43.elf
+radxa-zero3-rk3566_pkg := radxa-zero3
