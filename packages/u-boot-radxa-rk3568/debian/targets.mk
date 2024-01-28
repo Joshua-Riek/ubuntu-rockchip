78248752ff10e040119867b57f52504946bccc97
Joshua Riek <jjriek@verizon.net>
2024-01-28 13:19:44 -0500

    add the radxa zero3 to u-boot 2024.01

--- packages/u-boot-radxa-rk3568/debian/targets.mk
+++
@@ -1,7 +0,0 @@
-# Target platforms supported by u-boot.
-# debian/rules includes this Makefile snippet.
-
-u-boot-rockchip_platforms += radxa-zero3-rk3566
-radxa-zero3-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
-radxa-zero3-rk3566_bl31 := rk3568_bl31_set_pmic_sleep_low_20221018_v1.32.elf
-radxa-zero3-rk3566_pkg := radxa-zero3
