1ae6f769abf6038131523f7bfaba74e5f1f66713
Joshua Riek <jjriek@verizon.net>
2024-02-17 09:54:04 -0500

    add openssh-server package by default

--- config/projects/preinstalled-server.sh
+++ config/projects/preinstalled-server.sh
@@ -2,7 +2,7 @@
 
 package_list=(
     i2c-tools u-boot-tools mmc-utils flash-kernel wpasupplicant linux-firmware psmisc wireless-regdb
-    cloud-init landscape-common cloud-initramfs-growroot
+    cloud-init landscape-common cloud-initramfs-growroot openssh-server
 )
 
 package_removal_list=(
