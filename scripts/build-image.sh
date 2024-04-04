5c655e1d550c7925d6f9d30b00735519c7631c96
Joshua Riek <jjriek@verizon.net>
2024-04-03 23:02:35 -0400

    use a diffrent partition layout for ubuntu server and desktop

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -72,7 +72,7 @@ fi
 # Create an empty disk image
 img="../images/$(basename "${rootfs}" .rootfs.tar)${KVER}.img"
 size="$(( $(wc -c < "${rootfs}" ) / 1024 / 1024 ))"
-truncate -s "$(( size + 1024 + 512 ))M" "${img}"
+truncate -s "$(( size + 1024 ))M" "${img}"
 
 # Create loop device for disk image
 loop="$(losetup -f)"
@@ -88,72 +88,112 @@ umount "${disk}"* 2> /dev/null || true
 umount ${mount_point}/* 2> /dev/null || true
 mkdir -p ${mount_point}
 
-# Setup partition table
-dd if=/dev/zero of="${disk}" count=4096 bs=512
-parted --script "${disk}" \
-mklabel gpt \
-mkpart primary fat32 16MiB 20MiB \
-mkpart primary ext4 20MiB 100%
+if [ -z "${img##*server*}" ]; then
+    # Setup partition table
+    dd if=/dev/zero of="${disk}" count=4096 bs=512
+    parted --script "${disk}" \
+    mklabel gpt \
+    mkpart primary fat32 16MiB 20MiB \
+    mkpart primary ext4 20MiB 100%
 
-# Create partitions
-{
-    echo "t"
-    echo "1"
-    echo "BC13C2FF-59E6-4262-A352-B275FD6F7172"
-    echo "t"
-    echo "2"
-    echo "C12A7328-F81F-11D2-BA4B-00A0C93EC93B"
-    echo "w"
-} | fdisk "${disk}" &> /dev/null || true
+    # Create partitions
+    {
+        echo "t"
+        echo "1"
+        echo "BC13C2FF-59E6-4262-A352-B275FD6F7172"
+        echo "t"
+        echo "2"
+        echo "C12A7328-F81F-11D2-BA4B-00A0C93EC93B"
+        echo "w"
+    } | fdisk "${disk}" &> /dev/null || true
 
-partprobe "${disk}"
+    partprobe "${disk}"
 
-partition_char="$(if [[ ${disk: -1} == [0-9] ]]; then echo p; fi)"
+    partition_char="$(if [[ ${disk: -1} == [0-9] ]]; then echo p; fi)"
 
-sleep 1
+    sleep 1
 
-wait_loopdev "${disk}${partition_char}2" 60 || {
-    echo "Failure to create ${disk}${partition_char}1 in time"
-    exit 1
-}
+    wait_loopdev "${disk}${partition_char}2" 60 || {
+        echo "Failure to create ${disk}${partition_char}1 in time"
+        exit 1
+    }
 
-sleep 1
+    sleep 1
 
-wait_loopdev "${disk}${partition_char}1" 60 || {
-    echo "Failure to create ${disk}${partition_char}1 in time"
-    exit 1
-}
+    wait_loopdev "${disk}${partition_char}1" 60 || {
+        echo "Failure to create ${disk}${partition_char}1 in time"
+        exit 1
+    }
 
-sleep 1
+    sleep 1
 
-# Generate random uuid for bootfs
-boot_uuid=$(uuidgen | head -c8)
+    # Generate random uuid for bootfs
+    boot_uuid=$(uuidgen | head -c8)
 
-# Generate random uuid for rootfs
-root_uuid=$(uuidgen)
+    # Generate random uuid for rootfs
+    root_uuid=$(uuidgen)
 
-# Create filesystems on partitions
-mkfs.vfat -i "${boot_uuid}" -F32 -n CIDATA "${disk}${partition_char}1"
-dd if=/dev/zero of="${disk}${partition_char}2" bs=1KB count=10 > /dev/null
-mkfs.ext4 -U "${root_uuid}" -L cloudimg-rootfs "${disk}${partition_char}2"
+    # Create filesystems on partitions
+    mkfs.vfat -i "${boot_uuid}" -F32 -n CIDATA "${disk}${partition_char}1"
+    dd if=/dev/zero of="${disk}${partition_char}2" bs=1KB count=10 > /dev/null
+    mkfs.ext4 -U "${root_uuid}" -L cloudimg-rootfs "${disk}${partition_char}2"
 
-# Mount partitions
-mkdir -p ${mount_point}/{system-boot,writable} 
-mount "${disk}${partition_char}1" ${mount_point}/system-boot
-mount "${disk}${partition_char}2" ${mount_point}/writable
+    # Mount partitions
+    mkdir -p ${mount_point}/{system-boot,writable} 
+    mount "${disk}${partition_char}1" ${mount_point}/system-boot
+    mount "${disk}${partition_char}2" ${mount_point}/writable
+
+    # Cloud init config for server image
+    cp ../overlay/boot/firmware/{meta-data,user-data,network-config} ${mount_point}/system-boot
+else
+    # Setup partition table
+    dd if=/dev/zero of="${disk}" count=4096 bs=512
+    parted --script "${disk}" \
+    mklabel gpt \
+    mkpart primary ext4 16MiB 100%
+
+    # Create partitions
+    {
+        echo "t"
+        echo "1"
+        echo "C12A7328-F81F-11D2-BA4B-00A0C93EC93B"
+        echo "w"
+    } | fdisk "${disk}" &> /dev/null || true
+
+    partprobe "${disk}"
+
+    partition_char="$(if [[ ${disk: -1} == [0-9] ]]; then echo p; fi)"
+
+    sleep 1
+
+    wait_loopdev "${disk}${partition_char}1" 60 || {
+        echo "Failure to create ${disk}${partition_char}1 in time"
+        exit 1
+    }
+
+    sleep 1
+
+    # Generate random uuid for rootfs
+    root_uuid=$(uuidgen)
+
+    # Create filesystems on partitions
+    dd if=/dev/zero of="${disk}${partition_char}1" bs=1KB count=10 > /dev/null
+    mkfs.ext4 -U "${root_uuid}" -L desktop-rootfs "${disk}${partition_char}1"
+
+    # Mount partitions
+    mkdir -p ${mount_point}/writable
+    mount "${disk}${partition_char}1" ${mount_point}/writable
+
+    # Set boot args for the splash screen
+    bootargs="quiet splash plymouth.ignore-serial-consoles"
+fi
 
 # Copy the rootfs to root partition
 tar -xpf "${rootfs}" -C ${mount_point}/writable
 
-# Set boot args for the splash screen
-[ -z "${img##*desktop*}" ] && bootargs="quiet splash plymouth.ignore-serial-consoles" || bootargs=""
-
 # Create fstab entries
-mkdir -p ${mount_point}/writable/boot/firmware
-cat > ${mount_point}/writable/etc/fstab << EOF
-# <file system>     <mount point>  <type>  <options>   <dump>  <fsck>
-UUID=${root_uuid,,} /              ext4    defaults,x-systemd.growfs    0       1
-EOF
+echo "# <file system>     <mount point>  <type>  <options>   <dump>  <fsck>" > ${mount_point}/writable/etc/fstab
+echo "UUID=${root_uuid,,} /              ext4    defaults,x-systemd.growfs    0       1" >> ${mount_point}/writable/etc/fstab
 
 # Write bootloader to disk image
 if [ -f "${mount_point}/writable/usr/lib/u-boot/u-boot-rockchip.bin" ]; then
@@ -163,20 +203,7 @@ else
     dd if="${mount_point}/writable/usr/lib/u-boot/u-boot.itb" of="${loop}" seek=16384 conv=notrunc
 fi
 
-# Cloud init config for server image
-if [ -z "${img##*server*}" ]; then
-    cp ../overlay/boot/firmware/{meta-data,user-data,network-config} ${mount_point}/system-boot
-fi
-
-# Uboot env
-echo "console=ttyS2,1500000 console=tty1 rootwait rw cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory ${bootargs}" > ${mount_point}/writable/etc/kernel/cmdline
-
 # Mount the temporary API filesystems
-mkdir -p ${mount_point}/writable/{proc,sys,run,dev,dev/pts}
-mount -t proc /proc ${mount_point}/writable/proc
-mount -o bind /dev ${mount_point}/writable/dev
-mount -o bind /dev/pts ${mount_point}/writable/dev/pts
-
 touch ${mount_point}/writable/etc/kernel/cmdline
 mkdir -p ${mount_point}/writable/usr/share/u-boot-menu/conf.d/
 if [[ ${RELEASE} == "noble" ]]; then
@@ -199,11 +226,8 @@ U_BOOT_FDT_OVERLAYS_DIR="/usr/lib/linux-image-"
 EOF
 fi
 
-chroot ${mount_point}/writable/ /bin/bash -c "u-boot-update"
-
-# Umount temporary API filesystems
-umount -lf ${mount_point}/writable/dev/pts 2> /dev/null || true
-umount -lf ${mount_point}/writable/* 2> /dev/null || true
+# Uboot env
+echo "rootwait rw console=ttyS2,1500000 console=tty1 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory ${bootargs}" > ${mount_point}/writable/etc/kernel/cmdline
 
 # Run build image hook to handle board specific changes
 if [[ $(type -t build_image_hook__"${BOARD}") == function ]]; then
@@ -215,12 +239,14 @@ if [[ $(type -t build_image_hook__"${KERNEL_TARGET}") == function ]]; then
     build_image_hook__"${KERNEL_TARGET}"
 fi 
 
+chroot ${mount_point}/writable/ /bin/bash -c "u-boot-update"
+
 sync --file-system
 sync
 
 # Umount partitions
 umount "${disk}${partition_char}1"
-umount "${disk}${partition_char}2"
+umount "${disk}${partition_char}2" 2> /dev/null || true
 
 # Remove loop device
 losetup -d "${loop}"
