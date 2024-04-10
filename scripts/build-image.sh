0bba87c168e05e4e3608dbb3bd8e7921846cc46d
Joshua Riek <jjriek@verizon.net>
2024-04-10 16:35:03 -0400

    increase image compression ratio

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -239,6 +239,6 @@ losetup -d "${loop}"
 trap '' EXIT
 
 echo -e "\nCompressing $(basename "${img}.xz")\n"
-xz -3 --force --keep --quiet --threads=0 "${img}"
+xz -6 --force --keep --quiet --threads=0 "${img}"
 rm -f "${img}"
 cd ../images && sha256sum "$(basename "${img}.xz")" > "$(basename "${img}.xz.sha256")"
