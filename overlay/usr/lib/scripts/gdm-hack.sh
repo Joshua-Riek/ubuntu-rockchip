d913b096fa4906307b7458cf267cd9474efcf090
Joshua Riek <jjriek@verizon.net>
2024-05-05 14:44:19 -0400

    remove more unused rootfs overlay tweaks

--- overlay/usr/lib/scripts/gdm-hack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Hack for GDM to restart on first HDMI hotplug
-if systemctl is-active --quiet gdm && grep -Fxq "WaylandEnable=true" /etc/gdm3/custom.conf; then
-    if [ ! -f /tmp/gdm-wayland-session-hack.lock ]; then
-        touch /tmp/gdm-wayland-session-hack.lock && sleep 2 && pidof gdm-x-session > /dev/null && systemctl restart gdm
-    fi
-fi
