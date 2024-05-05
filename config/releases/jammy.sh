69229886863b5a5f82ffb1406646ddb4cf582e42
Joshua Riek <jjriek@verizon.net>
2024-05-05 13:52:19 -0400

    export release configs

--- config/releases/jammy.sh
+++ config/releases/jammy.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
-RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
-RELASE_VERSION="22.04"
+export RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
+export RELASE_VERSION="22.04"
 if [ -z "${KERNEL_TARGET}" ]; then
-    KERNEL_TARGET="rockchip-5.10"
+    export KERNEL_TARGET="rockchip-5.10"
 fi
