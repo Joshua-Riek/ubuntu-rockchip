0b0015a676cc990915faaf210788acc138e191f7
Joshua Riek <jjriek@verizon.net>
2024-06-01 14:29:15 -0400

    test build for radxa 8hd and 10hd displays

--- config/suites/jammy.sh
+++ config/suites/jammy.sh
@@ -4,4 +4,4 @@ export RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
 export RELASE_VERSION="22.04"
 
 export KERNEL_REPO=https://github.com/Joshua-Riek/linux-rockchip.git
-export KERNEL_BRANCH=jammy
+export KERNEL_BRANCH=jammy-radxa-display
