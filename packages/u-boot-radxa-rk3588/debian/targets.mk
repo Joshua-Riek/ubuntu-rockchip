f616472e6876b054314be7bd350ecb02c08ba9d4
Stephen Chen <stephen@radxa.com>
2024-06-01 09:29:17 +0800

    radxa-rock5: update bl31 and ddr blob to improve stability
    Signed-off-by: Stephen Chen <stephen@radxa.com>

--- packages/u-boot-radxa-rk3588/debian/targets.mk
+++ packages/u-boot-radxa-rk3588/debian/targets.mk
@@ -2,8 +2,8 @@
 # debian/rules includes this Makefile snippet.
 
 u-boot-rockchip_platforms += rock-5b-rk3588
-rock-5b-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-rock-5b-rk3588_bl31 := rk3588_bl31_v1.38.elf
+rock-5b-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5b-rk3588_bl31 := rk3588_bl31_v1.45.elf
 rock-5b-rk3588_pkg := rock-5b
 
 u-boot-rockchip_platforms += rock-5b-plus-rk3588
@@ -12,38 +12,38 @@ rock-5b-plus-rk3588_bl31 := rk3588_bl31_v1.45.elf
 rock-5b-plus-rk3588_pkg := rock-5b-plus
 
 u-boot-rockchip_platforms += rock-5a-rk3588s
-rock-5a-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-rock-5a-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+rock-5a-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5a-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 rock-5a-rk3588s_pkg := rock-5a
 
 u-boot-rockchip_platforms += rock-5a-spi-rk3588s
-rock-5a-spi-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-rock-5a-spi-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+rock-5a-spi-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5a-spi-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 rock-5a-spi-rk3588s_pkg := rock-5a-spi
 
 u-boot-rockchip_platforms += rock-5d-rk3588s
-rock-5d-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-rock-5d-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+rock-5d-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5d-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 rock-5d-rk3588s_pkg := rock-5d
 
 u-boot-rockchip_platforms += rock-5c-rk3588s
-rock-5c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-rock-5c-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+rock-5c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5c-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 rock-5c-rk3588s_pkg := rock-5c
 
 u-boot-rockchip_platforms += rock-5-itx-rk3588
-rock-5-itx-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-rock-5-itx-rk3588_bl31 := rk3588_bl31_v1.38.elf
+rock-5-itx-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5-itx-rk3588_bl31 := rk3588_bl31_v1.45.elf
 rock-5-itx-rk3588_pkg := rock-5-itx
 
 u-boot-rockchip_platforms += radxa-cm5-io-rk3588s
-radxa-cm5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-radxa-cm5-io-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+radxa-cm5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+radxa-cm5-io-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 radxa-cm5-io-rk3588s_pkg := radxa-cm5-io
 
 u-boot-rockchip_platforms += radxa-nx5-io-rk3588s
-radxa-nx5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-radxa-nx5-io-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+radxa-nx5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+radxa-nx5-io-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 radxa-nx5-io-rk3588s_pkg := radxa-nx5-io
 
 u-boot-rockchip_platforms += nanopi-r6s-rk3588s
