dc2633e30e2d854e479d731dc37e9f0e94bc8fc9
Joshua Riek <jjriek@verizon.net>
2024-06-11 16:53:36 -0400

    update all rk3588 boards to use the new ddr and spl blobs

--- packages/u-boot-mixtile-rk3588/debian/targets.mk
+++ packages/u-boot-mixtile-rk3588/debian/targets.mk
@@ -2,6 +2,6 @@
 # debian/rules includes this Makefile snippet.
 
 u-boot-rockchip_platforms += mixtile-blade3-rk3588
-mixtile-blade3-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-mixtile-blade3-rk3588_bl31 := rk3588_bl31_v1.38.elf
+mixtile-blade3-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+mixtile-blade3-rk3588_bl31 := rk3588_bl31_v1.45.elf
 mixtile-blade3-rk3588_pkg := mixtile-blade3
