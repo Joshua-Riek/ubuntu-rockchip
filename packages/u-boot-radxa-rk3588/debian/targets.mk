cab28b0443a57a0ce192a97113a506a5de1b2af0
Joshua Riek <jjriek@verizon.net>
2024-06-11 16:53:36 -0400

    update all rk3588 boards to use the new ddr and spl blobs

--- packages/u-boot-radxa-rk3588/debian/targets.mk
+++ packages/u-boot-radxa-rk3588/debian/targets.mk
@@ -52,46 +52,46 @@ radxa-nx5-io-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 radxa-nx5-io-rk3588s_pkg := radxa-nx5-io
 
 u-boot-rockchip_platforms += nanopi-r6s-rk3588s
-nanopi-r6s-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-nanopi-r6s-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+nanopi-r6s-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopi-r6s-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 nanopi-r6s-rk3588s_pkg := nanopi-r6s
 
 u-boot-rockchip_platforms += nanopi-r6c-rk3588s
-nanopi-r6c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-nanopi-r6c-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+nanopi-r6c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopi-r6c-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 nanopi-r6c-rk3588s_pkg := nanopi-r6c
 
 u-boot-rockchip_platforms += nanopc-t6-rk3588
-nanopc-t6-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-nanopc-t6-rk3588_bl31 := rk3588_bl31_v1.38.elf
+nanopc-t6-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopc-t6-rk3588_bl31 := rk3588_bl31_v1.45.elf
 nanopc-t6-rk3588_pkg := nanopc-t6
 
 u-boot-rockchip_platforms += lubancat-4-rk3588s
-lubancat-4-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-lubancat-4-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+lubancat-4-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+lubancat-4-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 lubancat-4-rk3588s_pkg := lubancat-4
 
 u-boot-rockchip_platforms += indiedroid-nova-rk3588s
-indiedroid-nova-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-indiedroid-nova-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+indiedroid-nova-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+indiedroid-nova-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 indiedroid-nova-rk3588s_pkg := indiedroid-nova
 
 u-boot-rockchip_platforms += armsom-w3-rk3588
-armsom-w3-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-armsom-w3-rk3588_bl31 := rk3588_bl31_v1.38.elf
+armsom-w3-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+armsom-w3-rk3588_bl31 := rk3588_bl31_v1.45.elf
 armsom-w3-rk3588_pkg := armsom-w3
 
 u-boot-rockchip_platforms += armsom-sige7-rk3588
-armsom-sige7-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-armsom-sige7-rk3588_bl31 := rk3588_bl31_v1.38.elf
+armsom-sige7-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+armsom-sige7-rk3588_bl31 := rk3588_bl31_v1.45.elf
 armsom-sige7-rk3588_pkg := armsom-sige7
 
 u-boot-rockchip_platforms += roc-rk3588s-pc-rk3588s
-roc-rk3588s-pc-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-roc-rk3588s-pc-rk3588s_bl31 := rk3588_bl31_v1.38.elf
+roc-rk3588s-pc-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+roc-rk3588s-pc-rk3588s_bl31 := rk3588_bl31_v1.45.elf
 roc-rk3588s-pc-rk3588s_pkg := roc-rk3588s-pc
 
 u-boot-rockchip_platforms += mixtile-core3588e-rk3588
-mixtile-core3588e-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
-mixtile-core3588e-rk3588_bl31 := rk3588_bl31_v1.38.elf
+mixtile-core3588e-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+mixtile-core3588e-rk3588_bl31 := rk3588_bl31_v1.45.elf
 mixtile-core3588e-rk3588_pkg := mixtile-core3588e
