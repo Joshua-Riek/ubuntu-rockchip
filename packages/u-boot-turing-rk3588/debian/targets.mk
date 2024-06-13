29550ca332c4c39af58816bdb353f2718cd42601
Joshua Riek <jjriek@verizon.net>
2024-06-12 20:11:43 -0400

    update the ddr and spl blobs for the Turing RK1

--- packages/u-boot-turing-rk3588/debian/targets.mk
+++ packages/u-boot-turing-rk3588/debian/targets.mk
@@ -2,8 +2,8 @@
 # debian/rules includes this Makefile snippet.
 
 u-boot-rockchip_platforms += turing-rk1-rk3588
-turing-rk1-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_uart9_115200_v1.11.bin
-turing-rk1-rk3588_bl31 := rk3588_bl31_v1.38.elf
+turing-rk1-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_uart9_115200_v1.16.bin
+turing-rk1-rk3588_bl31 := rk3588_bl31_v1.45.elf
 turing-rk1-rk3588_pkg := turing-rk1
 
 u-boot-rockchip_platforms += orangepi-3b-rk3566
