478ecc7b664328b7850ee4c1fe286cb3ebd3358e
Joshua Riek <jjriek@verizon.net>
2024-06-22 12:45:26 -0400

    fix rock 5a typo

--- config/boards/rock-5a.sh
+++ config/boards/rock-5a.sh
@@ -2,7 +2,7 @@
 
 export BOARD_NAME="ROCK 5A"
 export BOARD_MAKER="Radxa"
-export BOARD_SOC="Rockchip RK3588"
+export BOARD_SOC="Rockchip RK3588S"
 export BOARD_CPU="ARM Cortex A76 / A55"
 export UBOOT_PACKAGE="u-boot-radxa-rk3588"
 export UBOOT_RULES_TARGET="rock-5a-rk3588s"
