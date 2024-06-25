c388d066d5dabf51e300608ebda6795f6648a371
Joshua Riek <jjriek@verizon.net>
2024-06-25 19:07:49 -0400

    import ubuntu live build code

--- config/flavors/server.sh
+++ config/flavors/server.sh
@@ -1,1 +1,3 @@
 # shellcheck shell=bash
+
+export PROJECT=ubuntu-cpc
--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -31,8 +31,122 @@ if [[ -f ubuntu-${RELASE_VERSION}-preinstalled-${FLAVOR}-arm64.rootfs.tar.xz ]];
     exit 0
 fi
 
-git clone https://github.com/Joshua-Riek/ubuntu-live-build.git
-cd ubuntu-live-build
-bash ./docker/build-livecd-rootfs.sh
-bash ./build.sh "--${FLAVOR}" "--${SUITE}"
-mv "./build/ubuntu-${RELASE_VERSION}-preinstalled-${FLAVOR}-arm64.rootfs.tar.xz" ../
+pushd .
+
+tmp_dir=$(mktemp -d)
+cd "${tmp_dir}" || exit 1
+
+# Clone the livecd rootfs fork
+git clone https://github.com/Joshua-Riek/livecd-rootfs
+cd livecd-rootfs || exit 1
+
+# Install build deps
+apt-get update
+apt-get build-dep . -y
+
+# Build the package
+dpkg-buildpackage -us -uc
+
+# Install the custom livecd rootfs package
+apt-get install ../livecd-rootfs_*.deb --assume-yes --allow-downgrades --allow-change-held-packages
+dpkg -i ../livecd-rootfs_*.deb
+apt-mark hold livecd-rootfs
+
+rm -rf "${tmp_dir}"
+
+popd
+
+mkdir -p live-build && cd live-build
+
+# Query the system to locate livecd-rootfs auto script installation path
+cp -r "$(dpkg -L livecd-rootfs | grep "auto$")" auto
+
+set +e
+
+export ARCH=arm64
+export IMAGEFORMAT=none
+export IMAGE_TARGETS=none
+export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
+
+# Populate the configuration directory for live build
+lb config \
+    --architecture arm64 \
+    --bootstrap-qemu-arch arm64 \
+    --bootstrap-qemu-static /usr/bin/qemu-aarch64-static \
+    --archive-areas "main restricted universe multiverse" \
+    --parent-archive-areas "main restricted universe multiverse" \
+    --mirror-bootstrap "http://ports.ubuntu.com" \
+    --parent-mirror-bootstrap "http://ports.ubuntu.com" \
+    --mirror-chroot-security "http://ports.ubuntu.com" \
+    --parent-mirror-chroot-security "http://ports.ubuntu.com" \
+    --mirror-binary-security "http://ports.ubuntu.com" \
+    --parent-mirror-binary-security "http://ports.ubuntu.com" \
+    --mirror-binary "http://ports.ubuntu.com" \
+    --parent-mirror-binary "http://ports.ubuntu.com" \
+    --keyring-packages ubuntu-keyring \
+    --linux-flavours rockchip
+
+# Pin rockchip package archives
+(
+    echo "Package: *"
+    echo "Pin: release o=LP-PPA-jjriek-rockchip"
+    echo "Pin-Priority: 1001"
+    echo ""
+    echo "Package: *"
+    echo "Pin: release o=LP-PPA-jjriek-rockchip-multimedia"
+    echo "Pin-Priority: 1001"
+) > config/archives/extra-ppas.pref.chroot
+
+if [ "${SUITE}" == "noble" ]; then
+    # Ignore custom ubiquity package (mistake i made, uploaded to wrong ppa)
+    (
+        echo "Package: oem-*"
+        echo "Pin: release o=LP-PPA-jjriek-rockchip-multimedia"
+        echo "Pin-Priority: -1"
+        echo ""
+        echo "Package: ubiquity*"
+        echo "Pin: release o=LP-PPA-jjriek-rockchip-multimedia"
+        echo "Pin-Priority: -1"
+
+    ) > config/archives/extra-ppas-ignore.pref.chroot
+fi
+
+# Snap packages to install
+(
+    echo "snapd/classic=stable"
+    echo "core22/classic=stable"
+    echo "lxd/classic=stable"
+) > config/seeded-snaps
+
+# Generic packages to install
+(
+    echo "rockchip-multimedia-config"
+    echo "software-properties-common"
+    echo "linux-firmware"
+) > config/package-lists/my.list.chroot
+
+if [ "${PROJECT}" == "ubuntu" ]; then
+    # Specific packages to install for ubuntu desktop
+    (
+        echo "ubuntu-desktop-rockchip"
+        echo "oem-config-gtk"
+        echo "ubiquity-frontend-gtk"
+        echo "ubiquity-slideshow-ubuntu"
+        echo "gstreamer1.0-rockchip1"
+        echo "chromium-browser"
+        echo "libv4l-rkmpp"
+        echo "localechooser-data"
+    ) >> config/package-lists/my.list.chroot
+else
+    # Specific packages to install for ubuntu server
+    echo "ubuntu-server-rockchip" >> config/package-lists/my.list.chroot
+fi
+
+# Build the rootfs
+lb build
+
+set -eE 
+
+# Tar the entire rootfs
+(cd chroot/ &&  tar -p -c --sort=name --xattrs ./*) | xz -3 -T0 > "ubuntu-${RELASE_VERSION}-preinstalled-${FLAVOR}-arm64.rootfs.tar.xz"
+mv "ubuntu-${RELASE_VERSION}-preinstalled-${FLAVOR}-arm64.rootfs.tar.xz" ../
