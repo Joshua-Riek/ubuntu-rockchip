0c049f0ff564afda028d5ccc669ee511708c77b1
Joshua Riek <jjriek@verizon.net>
2024-06-29 14:29:48 -0400

    add U-Boot support for the ArmSoM Sige5

---
+++ packages/u-boot-armsom-rk3576/debian/targets.mk
@@ -0,0 +1,8 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += armsom-sige5-rk3576
+armsom-sige5-rk3576_ddr := rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.03.bin
+armsom-sige5-rk3576_bl31 := rk3576_bl31_v1.04.elf
+armsom-sige5-rk3576_bl32 := rk3576_bl32_v1.01.bin
+armsom-sige5-rk3576_pkg := armsom-sige5
