5e2f40546a0c556fcb6f200247c4b58840cd5546
Joshua Riek <jjriek@verizon.net>
2024-06-29 13:41:29 -0400

    use next kernel branch for testing

--- config/suites/noble.sh
+++ config/suites/noble.sh
@@ -4,4 +4,4 @@ export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
 export RELASE_VERSION="24.04"
 
 export KERNEL_REPO=https://github.com/Joshua-Riek/linux-rockchip.git
-export KERNEL_BRANCH=noble
+export KERNEL_BRANCH=next
