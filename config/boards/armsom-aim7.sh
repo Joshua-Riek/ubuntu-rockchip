75340b16e6cee262300a3ef635cf97869010079d
Joshua Riek <jjriek@verizon.net>
2024-06-29 13:40:24 -0400

    add board configs for the ArmSoM Sige5 and AIM7

---
+++ config/boards/armsom-aim7.sh
@@ -0,0 +1,26 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="ArmSoM AIM7"
+export BOARD_MAKER="ArmSoM"
+export BOARD_SOC="Rockchip RK3588"
+export BOARD_CPU="ARM Cortex A76 / A55"
+export UBOOT_PACKAGE="u-boot-radxa-rk3588"
+export UBOOT_RULES_TARGET="armsom-aim7-rk3588"
+
+function config_image_hook__armsom-aim7() {
+    local rootfs="$1"
+
+    # Install panfork
+    chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
+    chroot "${rootfs}" apt-get update
+    chroot "${rootfs}" apt-get -y install mali-g610-firmware
+    chroot "${rootfs}" apt-get -y dist-upgrade
+
+    # Install libmali blobs alongside panfork
+    chroot "${rootfs}" apt-get -y install libmali-g610-x11
+
+    # Install the rockchip camera engine
+    chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3588
+
+    return 0
+}
---
+++ config/boards/armsom-sige5.sh
@@ -0,0 +1,13 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="ArmSoM Sige5"
+export BOARD_MAKER="ArmSoM"
+export BOARD_SOC="Rockchip RK3576"
+export BOARD_CPU="ARM Cortex A76 / A55"
+export UBOOT_PACKAGE="u-boot-radxa-rk3588"
+export UBOOT_RULES_TARGET="armsom-sige5-rk3588"
+
+function config_image_hook__armsom-sige5() {
+
+    return 0
+}
