50e63a41c669e76917057d64e2609ea631e2dd7e
Joshua Riek <jjriek@verizon.net>
2024-07-06 13:03:22 -0400

    Update RK3576 upstream and refactor U-Boot

--- config/boards/armsom-sige5.sh
+++ config/boards/armsom-sige5.sh
@@ -4,7 +4,7 @@ export BOARD_NAME="ArmSoM Sige5"
 export BOARD_MAKER="ArmSoM"
 export BOARD_SOC="Rockchip RK3576"
 export BOARD_CPU="ARM Cortex A76 / A55"
-export UBOOT_PACKAGE="u-boot-armsom-rk3576"
+export UBOOT_PACKAGE="u-boot-rk3576"
 export UBOOT_RULES_TARGET="armsom-sige5-rk3576"
 
 function config_image_hook__armsom-sige5() {
