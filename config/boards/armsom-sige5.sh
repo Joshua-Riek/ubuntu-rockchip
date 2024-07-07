47e11adb4453e103af72a2ed62bfbba3f813de45
Joshua Riek <jjriek@verizon.net>
2024-07-07 16:11:57 -0400

    blacklist panfrost on the ArmSoM Sige5

--- config/boards/armsom-sige5.sh
+++ config/boards/armsom-sige5.sh
@@ -8,6 +8,10 @@ export UBOOT_PACKAGE="u-boot-rk3576"
 export UBOOT_RULES_TARGET="armsom-sige5-rk3576"
 
 function config_image_hook__armsom-sige5() {
+    local rootfs="$1"
+
+    # Kernel modules to blacklist
+    echo "blacklist panfrost" > "${rootfs}/etc/modprobe.d/panfrost.conf"
 
     return 0
 }
