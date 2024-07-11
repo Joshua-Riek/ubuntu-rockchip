4c301d5841736eb7812b04bef87c82f4418839fd
Joshua Riek <jjriek@verizon.net>
2024-07-11 18:47:13 -0400

    Move Orange Pi 5 Plus / 5 Pro to Radxa's U-Boot

--- config/boards/orangepi-5b.sh
+++ config/boards/orangepi-5b.sh
@@ -4,8 +4,8 @@ export BOARD_NAME="Orange Pi 5B"
 export BOARD_MAKER="Xulong"
 export BOARD_SOC="Rockchip RK3588S"
 export BOARD_CPU="ARM Cortex A76 / A55"
-export UBOOT_PACKAGE="u-boot-orangepi-rk3588"
-export UBOOT_RULES_TARGET="orangepi_5b"
+export UBOOT_PACKAGE="u-boot-radxa-rk3588"
+export UBOOT_RULES_TARGET="orangepi-5b-rk3588s"
 
 function config_image_hook__orangepi-5b() {
     local rootfs="$1"
