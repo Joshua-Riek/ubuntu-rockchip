c08bb857aa5532c226c9d02d4458ffc17c6b5c3a
serwansj <44338225+serwansj@users.noreply.github.com>
2024-07-25 22:10:39 +0000

    Add necessary configs and patch for u-boot to support AIO-3588L
    Co-authored-by: Joshua Riek <jjriek@verizon.net>

---
+++ config/boards/aio-3588l.sh
@@ -0,0 +1,28 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="AIO 3588L"
+export BOARD_MAKER="Firefly"
+export BOARD_SOC="Rockchip RK3588"
+export BOARD_CPU="ARM Cortex A76 / A55"
+export UBOOT_PACKAGE="u-boot-radxa-rk3588"
+export UBOOT_RULES_TARGET="aio-3588l-rk3588"
+#export UBOOT_PACKAGE="u-boot-radxa-rk3588"
+#export UBOOT_RULES_TARGET="roc-rk3588s-pc-rk3588s"
+
+function config_image_hook__aio-3588l() {
+    local rootfs="$1"
+
+    # Install panfork
+    chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
+    chroot "${rootfs}" apt-get update
+    chroot "${rootfs}" apt-get -y install mali-g610-firmware
+    chroot "${rootfs}" apt-get -y dist-upgrade
+
+    # Install libmali blobs alongside panfork
+    chroot "${rootfs}" apt-get -y install libmali-g610-x11
+
+    # Install the rockchip camera engine
+    chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3588
+
+    return 0
+}
