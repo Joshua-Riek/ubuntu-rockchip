6aed4b74187d8a582f786e400586b531b8008e89
Joshua Riek <jjriek@verizon.net>
2024-08-03 11:21:47 -0400

    Add suite for Ubuntu 24.10

---
+++ config/suites/oracular.sh
@@ -0,0 +1,7 @@
+# shellcheck shell=bash
+
+export RELASE_NAME="Ubuntu 24.10 (Oracular Oriole)"
+export RELASE_VERSION="24.10"
+
+export KERNEL_REPO=https://github.com/Joshua-Riek/linux.git
+export KERNEL_BRANCH=v6.11-rc1-rk3588
