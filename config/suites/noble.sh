84967db20cee2139e82f3c08326cd8b3555e1024
Nicolas Pereira <hqnicolas@msn.com>
2025-01-06 22:20:53 -0300

    mod: kernel origin

--- config/suites/noble.sh
+++ config/suites/noble.sh
@@ -3,7 +3,7 @@
 export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
 export RELASE_VERSION="24.04"
 
-export KERNEL_REPO="https://github.com/Joshua-Riek/linux-rockchip.git"
+export KERNEL_REPO="https://github.com/hqnicolas/linux-rockchip.git"
 export KERNEL_BRANCH="noble"
 export KERNEL_FLAVOR="rockchip"
 
--- config/suites/oracular.sh
+++ config/suites/oracular.sh
@@ -3,7 +3,7 @@
 export RELASE_NAME="Ubuntu 24.10 (Oracular Oriole)"
 export RELASE_VERSION="24.10"
 
-export KERNEL_REPO="https://github.com/Joshua-Riek/linux-rockchip.git"
+export KERNEL_REPO="https://github.com/hqnicolas/linux-rockchip.git"
 export KERNEL_BRANCH="oracular"
 export KERNEL_FLAVOR="rockchip"
 
