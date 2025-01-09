f9d3c608311f0f4fd46a50602d3382129f91d761
ZeaKyX <43315078+ZeaKyX@users.noreply.github.com>
2025-01-09 17:17:43 +0800

    fix RK3576 CPU spec typo

--- config/boards/armsom-sige5.sh
+++ config/boards/armsom-sige5.sh
@@ -3,7 +3,7 @@
 export BOARD_NAME="ArmSoM Sige5"
 export BOARD_MAKER="ArmSoM"
 export BOARD_SOC="Rockchip RK3576"
-export BOARD_CPU="ARM Cortex A76 / A55"
+export BOARD_CPU="ARM Cortex A72 / A53"
 export UBOOT_PACKAGE="u-boot-rk3576"
 export UBOOT_RULES_TARGET="armsom-sige5-rk3576"
 export COMPATIBLE_SUITES=("noble")
